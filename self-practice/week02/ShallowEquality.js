da3091d4bf8f6f67aa0f475fb1330e1e7c67c476
Thanasorn Dusadeeroj <64414782vavaza09@user.norelpy.github.com>
2025-08-18 22:59:31 +0700

    finish week02 ShallowEquality.js and progression02.txt

--- self-practice/week02/ShallowEquality.js
+++ self-practice/week02/ShallowEquality.js
@@ -1,1 +1,10 @@
 //comparision object with [key]
+function comparisoinObject(obj1, obj2){
+  //ใช้ Object.entries เพื่อแยก key และ value ออกมาแล้วนำไปเช็คโดยใช้ every โดยเช็คจาก key ว่ามี vaule เดียวกันไหม
+  return Object.entries(obj1).every(([key, value]) => obj2[key] === value);
+}
+
+obj1 = {id: 1, fullname: "obj1", age: 19}
+obj2 = {ids: 1, fullname: "obj1", age: 19}
+
+console.log(comparisoinObject(obj1, obj2));
